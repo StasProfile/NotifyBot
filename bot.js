fd6d2f582dd15ceb3c229f0df74c10077c9d9713
StasProfile <stas.babkin.88@mail.ru>
2020-11-23 22:49:39 +0300

    fixed notification list

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const { match } = require('assert');
 const dotenv = require('dotenv').config();
+const schedule = require('node-schedule');
 var TelegramBot = require('node-telegram-bot-api');
 
     const token = process.env.BOT_TOKEN;
 	const bot = new TelegramBot(token, {polling: true});
 
-	let notes = [];
-    let notifList = '';
+	// let notes = [];
+    const users = {};
 
     bot.onText(/\/help/, function (msg) {
         const userId = msg.from.id;
@@ -17,52 +18,82 @@ var TelegramBot = require('node-telegram-bot-api');
 	  const userId = msg.from.id;
 	  const text = match[1];
 	  const time = match[2];
+    //   notes.push( { uid:userId, time:time, text:text } );
 
-	  notes.push( { uid:userId, time:time, text:text } );
-      notifList += (notes.length) + ') ' + notes[notes.length - 1].text + ' в ' + notes[notes.length - 1].time + '\n';
-	  bot.sendMessage(userId,'Отлично! Я обязательно напомню, если не сдохну :)');
+      if (!users.hasOwnProperty(userId)) { 
+            users[userId] = [];
+        }
+    users[userId].push({message : text, time});
+
+      bot.sendMessage(userId,'Отлично! Я обязательно напомню, если не сдохну :)');
+    
+      const [hours,minutes] = match[2].split(':');
+      const now = new Date();
+      const date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes, 0);
+      
+      const notification = schedule.scheduleJob(date, function(){
+        bot.sendMessage(userId, 'Напоминаю, что вы должны: '+ text + ' сейчас.');
+        if (users[userId].length === 1) {
+            users[userId] = null;
+        }
+        else {
+            users[userId].splice(users[userId].findIndex((note) => note.text === text && note.time === time),1);
+        }
+        notification.cancel();
+      });
+      
+
     });
 
-    bot.onText(/\/уведомления/, function (msg, match) {
-        if (notes.length === 0) {
+    bot.onText(/\/уведомления/, function (msg) {
+        const notes = users[msg.from.id];
+        if (!notes) {
             bot.sendMessage(msg.from.id,'Список уведомление пуст');
         } 
         else {
-            bot.sendMessage(msg.from.id, notifList);
+            const str = notes.map((note, index) => `${index + 1}) ${note.message} ${note.time}`).join('\n');
+            bot.sendMessage(msg.from.id, str);
         }
     });
     
     bot.onText(/\/удалить (.+)/, function (msg, match) {
         const userId = msg.from.id;
-        if (match[1] === 'все' || match[1] === 'всё') {
-            notes = [];
-            notifList = '';
-            bot.sendMessage(userId, 'все уведомления удалены');
-        } 
-        else {
-            number = match[1] - 1;
-            console.log(number);
-            if (number > notes.length - 1){
-                bot.sendMessage(userId, 'не существует уведомления с таким номером\n' + notifList);
-            }
-            else {
-                notes.splice(number,1);
-                notifList = '';
-                for (let i = 0; i < notes.length; i++) { 
-                    notifList += (i + 1) + ') ' + notes[i].text + ' в ' + notes[i].time + '\n';
-                }
-                bot.sendMessage(userId, 'удалено, новый список уведомлений:\n' + notifList);
-            }
+        let notes = users[userId];
+        if (!notes) {
+            return bot.sendMessage(userId, 'нечего удалять');
         }
+        if (match[1] === 'все' || match[1] === 'всё') {
+            users[userId] = null;
+         return bot.sendMessage(userId, 'все уведомления удалены');
+        } 
+        const number = match[1] - 1;
+        const str = notes.map((note, index) => `${index + 1}) ${note.message} ${note.time}`).join('\n');
+        if (number > notes.length - 1){
+          
+           return bot.sendMessage(userId, 'не существует уведомления с таким номером\n' + str);
+        }
+        
+        if (notes.length === 1) {
+            notes = null;
+          return  bot.sendMessage(userId, 'уведомление удалено, список уведомлений пуст:\n');
+        } 
+    
+        notes.splice(number,1);               //ловушка джокера
+        const newMessage = notes.map((note, index) => `${index + 1}) ${note.message} ${note.time}`).join('\n');
+        bot.sendMessage(userId, 'уведомление удалено, новый список уведомлений:\n' + newMessage);
+        
     });
 
 
-	setInterval(function(){
-		for (var i = 0; i < notes.length; i++){
-			let curDate = new Date().getHours() + ':' + new Date().getMinutes();
-				if ( notes[i].time == curDate ) {
-					bot.sendMessage(notes[i].uid, 'Напоминаю, что вы должны: '+ notes[i].text + ' сейчас.');
-					notes.splice(i,1);
-				}
-			}
-	},1000);
+	// setInterval(function(){
+	// 	for (var i = 0; i < notes.length; i++){
+	// 		let curDate = new Date().getHours() + ':' + new Date().getMinutes();
+	// 			if ( notes[i].time == curDate ) {
+	// 				bot.sendMessage(notes[i].uid, 'Напоминаю, что вы должны: '+ notes[i].text + ' сейчас.');
+	// 				notes.splice(i,1);
+	// 			}
+	// 		}
+    // },1000);
+    
+    
+     
