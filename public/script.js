e010f06a9ace1b531b5f6e3fc4d6fdb9b6f4e8ec
StasProfile <sbabkin@sfedu.ru>
2021-05-12 00:00:56 +0300

    patch button added

--- public/script.js
+++ public/script.js
@@ -58,6 +58,49 @@ const showList = async (userId) => {
   notes.forEach(note => {
     const newElement = document.createElement('div');
     newElement.className = 'notification';
+    
+    const editIcon = document.createElement('span');
+    editIcon.className = 'notification__edit';
+    
+    editIcon.addEventListener('click', async function() {
+      //@todo remove "x" button
+      this.parentElement.removeChild(closeIcon);
+      this.remove();
+      //??????
+
+      const editIconDiscard = await document.createElement('span');
+      editIconDiscard.className = 'notification__edit__discard';
+      editIconDiscard.innerText = 'Отменить';
+      this.parentElement.insertAdjacentElement('beforeend', editIconDiscard)
+    });
+
+      editIconDiscard.addEventListener('click', async function() {
+        //@todo
+        const respGet = await fetch(`notifications/${userId}/${note._id}`);
+        const note = await respGet.json();
+        //@todo
+      });
+
+      const editIconSubmit = await document.createElement('span');
+      editIconSubmit.className = 'notification__edit__submit'
+      editIconSubmit.innerText = 'Подтвердить';
+      await newElement.insertAdjacentElement('beforeend', editIconSubmit)
+      
+      const editMessage = document.createElement('input', 'type="text-area" cols="40" rows="3" id="notificationId"')
+
+      editIconSumbit.addEventListener('click', async function() {
+        const respPatch = await fetch(`notifications/${userId}/${note._id}`, { 
+          method: 'patch',
+          body: JSON.stringify({
+            message: String,
+            date: Date,
+            }),
+        });
+      });
+      
+
+  
+      
 
     const closeIcon = document.createElement('span');
     closeIcon.className = 'notification__close';
@@ -71,9 +114,11 @@ const showList = async (userId) => {
     });
     
     closeIcon.innerText = 'x';
-
+    editIcon.innerText = 'Изменить';
 
     newElement.innerText = `${note.message} в ${createDateTime(new Date(note.date))}`;
+    
+    newElement.insertAdjacentElement('beforeend', editIcon);
     newElement.insertAdjacentElement('beforeend', closeIcon)
 
     elementPlace.insertAdjacentElement('beforeend', newElement);
